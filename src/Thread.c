c8dc2453486cfbdbe896af115c3f0244aa2243a4
dylanlosh <dylan.losh@eagles.oc.edu>
2018-09-04 08:39:05 -0500

    added gitignores and keil files
    added gitignores and keil files

---
+++ src/Thread.c
@@ -0,0 +1,69 @@
+
+#include "cmsis_os.h"  // CMSIS RTOS header file
+#include "Board_LED.h"
+#include "UART_driver.h"
+#include "stdint.h"                     // data type definitions
+#include "stdio.h"                      // file I/O functions
+#include "rl_usb.h"                     // Keil.MDK-Pro::USB:CORE
+#include "rl_fs.h"                      // Keil.MDK-Pro::File System:CORE
+#include "stm32f4xx_hal.h"
+#include "stm32f4_discovery.h"
+#include "stm32f4_discovery_audio.h"
+#include "math.h"
+#include "arm_math.h" // header for DSP library
+#include <stdio.h>
+
+// LED constants
+#define LED_Green   0
+#define LED_Orange  1
+#define LED_Red     2
+#define LED_Blue    3
+
+void Thread_1 (void const *arg);                           // function prototype for Thread_1
+osThreadDef (Thread_1, osPriorityNormal, 1, 0);            // define Thread_1
+
+void Init_Thread (void) {
+
+	osThreadId id; // holds the returned thread create ID
+	
+	LED_Initialize(); // Initialize the LEDs
+	UART_Init(); // Initialize the UART
+	
+  id = osThreadCreate (osThread (Thread_1), NULL);         // create the thread
+  if (id == NULL) {                                        // handle thread creation
+    // Failed to create a thread
+  };
+}
+
+// Thread function
+void Thread_1(void const *arg){
+
+	while(1){
+	int x = 1;
+		while(x <= 100){
+x=x+2;
+		
+		LED_On(0);
+		LED_Off(1);
+		LED_Off(2);
+		LED_Off(3);
+		osDelay (x*100);
+		LED_Off(0);
+		LED_On(1);
+		LED_Off(2);
+		LED_Off(3);
+		osDelay (x*100);
+		LED_Off(0);
+		LED_Off(1);
+		LED_On(2);
+		LED_Off(3);
+		osDelay(x*100);
+		LED_Off(0);
+		LED_Off(1);
+		LED_Off(2);
+		LED_On(3);
+		osDelay(x*100);
+	}
+}
+}
+
